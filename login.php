de10ebde7187a2daec089b19657383562709ffe4
Yoonmie <80238605+Yoonmie@users.noreply.github.com>
2021-03-08 19:04:59 +0630

    Update login.php

--- login.php
+++ login.php
@@ -1,24 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <title>Log In Form</title>
-  <style>
-  </style>
-</head>
-<body class="bg-light">
 <?php
- 
+session_start();
 if(isset($_POST['btnlogin'])) {
   $email=$_POST['email'];
   $password =$_POST['pw'];
-  if($email!=null && $password!=null) 
+  if($email!=null && $password!=null)
   {
     if($email=="admin@gmail.com" && $password=="123456")
     {
+      $_SESSION['login']="login";
       header("Location: home.php");
     } 
     else{
@@ -30,6 +20,18 @@ if(isset($_POST['btnlogin'])) {
   }
 }
 ?>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <title>Log In Form</title>
+  <style>
+  </style>
+</head>
+<body class="bg-light">
+
 <form action="login.php" method="post">
   <div class="container border rounded-lg col-lg-6  col-sm-10  mt-5">
   <div class="form-group mt-3">
