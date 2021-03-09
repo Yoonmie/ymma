c42e43380d543d7b3cdffa8126d10d0ff42df88c
Yoonmie <80238605+Yoonmie@users.noreply.github.com>
2021-03-09 14:29:14 +0630

    Update logout.php

--- logout.php
+++ logout.php
@@ -7,7 +7,7 @@ echo "<script type='text/javascript'>alert('Log Out Successfully!')</script>";
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Log Out Form</title>
 </head>
 <body>
 
